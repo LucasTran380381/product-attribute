7c04486e2912cdbc47913408ac6537c052dd1417
Lukas Tran <trannhan380381@gmail.com>
2024-10-23 16:09:16 +0700

    [MIG] partner_logistics_uom: Migration to 18.0

--- product_logistics_uom/models/product_product.py
+++ product_logistics_uom/models/product_product.py
@@ -14,8 +14,8 @@ class ProductProduct(models.Model):
     # volume and weight UOM. In the same time, we need to keep the volume
     # we ensure that no information is lost by storing the volume and weight
     # without rounding.
-    volume = fields.Float(digits=False)
-    weight = fields.Float(digits=False)
+    volume = fields.Float()
+    weight = fields.Float()
 
     product_volume = fields.Float(
         "Volume in product UOM",
